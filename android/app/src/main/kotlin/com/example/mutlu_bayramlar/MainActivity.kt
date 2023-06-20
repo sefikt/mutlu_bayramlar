3f773b28e8e8c15c23786ada78ff260ddc8ef81e
sefikt <sefiktoru72@gmail.com>
2023-06-20 11:49:08 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/mutlu_bayramlar/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mutlu_bayramlar
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Color(0xffD7E0FF),
+        appBar: AppBar(
+          centerTitle: true,
+          backgroundColor: Colors.blueGrey[800],
+          title: Text('Mutlu Bayramlar'),
+        ),
+        body: Center(child: Image.asset('images/mutlu_bayramlar.jpg')),
+      ),
+    ),
+  );
+}
